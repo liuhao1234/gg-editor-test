6dce78605a24035107d78de47bac518055597ce8
liuhao1234 <728672190@qq.com>
2019-01-21 10:25:21 +0800

    Revert "Revert "build项目""
    This reverts commit f588ffded522d627ea533ba3790396505ac6ec62.

---
+++ build/precache-manifest.64c6fa4e22bdc3fe0f29fa0e5ddf03b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce1e3a1c17fdd81451bd",
+    "url": "/static/js/main.ce1e3a1c.chunk.js"
+  },
+  {
+    "revision": "803ee0c703c0cc9db5d3",
+    "url": "/static/js/1.803ee0c7.chunk.js"
+  },
+  {
+    "revision": "ce1e3a1c17fdd81451bd",
+    "url": "/static/css/main.17af194d.chunk.css"
+  },
+  {
+    "revision": "803ee0c703c0cc9db5d3",
+    "url": "/static/css/1.eb3b6de0.chunk.css"
+  },
+  {
+    "revision": "af040b787bba7193acd4f44a9c7770d1",
+    "url": "/index.html"
+  }
+];
